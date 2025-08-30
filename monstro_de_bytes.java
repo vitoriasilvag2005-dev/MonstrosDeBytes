effae50989400d4c5c619fc45ea257f88dcd64ef
vitoriasilvag2005-dev <vitoriasilvag2005@gmail.com>
2025-08-30 12:44:03 -0300

    Update monstro_de_bytes.java

--- monstro_de_bytes.java
+++ monstro_de_bytes.java
@@ -15,13 +15,9 @@ public class PrimeiroRobo extends Robot
 	 */
 	public void run() {
 		// Initialization of the robot should be put here
+		out.println("PrimeiroRobo iniciado! Teste de pipeline funcionando! 🚀"); // mensagem de teste
 
-		// Teste do pipeline antes de push
-		// Teste do pipeline
-
-		// After trying out your robot, try uncommenting the import at the top,
-		// and the next line:
-		setColors(Color.green,Color.green,Color.blue); // body,gun,radar
+		setColors(Color.green, Color.green, Color.blue); // body, gun, radar
 
 		// Robot main loop
 		while(true) {
@@ -37,21 +33,21 @@ public class PrimeiroRobo extends Robot
 	 * onScannedRobot: What to do when you see another robot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
 		fire(2);
+		out.println("Inimigo detectado! Atirando!"); // mensagem de teste
 	}
 
 	/**
 	 * onHitByBullet: What to do when you're hit by a bullet
 	 */
 	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
+		out.println("Ai! Fui atingido!"); // mensagem de teste
 	}
 	
 	/**
 	 * onHitWall: What to do when you hit a wall
 	 */
 	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
+		out.println("Bateu na parede!"); // mensagem de teste
 	}	
 }
